c9b26fd24d152657905f8216b367508ee2e938d5
lawless-m <maht0x0r@xsmail.com>
2016-08-29 10:39:38 -0400

    Revert "using Formatting"
    This reverts commit 330e77e99cd437f2eb9e54e4db775e8be5c0c6ca.

--- Bay2Bay_Costs.jl
+++ Bay2Bay_Costs.jl
@@ -2,42 +2,16 @@
 create grid for excel to view
 =#
 
-using Formatting
-
-tab = open("PathingCosts.txt", "w")
-img = open("PathingMap.pgm", "w")
-dim = 0
-
-macro pfn(fn, arg)
-	return quote
-		if typeof($arg) == ASCIIString
-			printfmt($fn, "{:s}", $arg)
-		else
-			printfmt($fn, $arg[1], $arg[2:end]...)
-		end
-	end
-end
-
-macro ptab(arg)
-	:(@pfn tab $arg)
-end
-macro pimg(arg)
-	:(@pfn img $arg)
-end
-
-
-@ptab "\n"
+tab = open("Bay2Bay.txt", "w")
+@printf tab "\t"
 for rackH in 81:-1:1, binH in 8:-1:1, levelH in [10:10:90; 91]
-	dim += 1
-	@ptab "F-{:02d}-{:02d}-{:02d}\t", rackH, levelH, binH
+	@printf tab "F-%02d-%02d-%02d\t" rackH levelH binH
 end
-@ptab "\n"
+@printf tab "\n"
 
-@pimg "P5 {:d} {:d} 127", dim, dim
-
-macro pcon(fmt)
+macro pcon(str)
 	return quote
-		@ptab $fmt
+		write(tab, $str)
 		continue
 	end
 end
@@ -60,33 +34,32 @@ inAisleDists = [0 1 1 2 2 1
 			  1 1 1 1 1 0]
 			  
 for rackV in 81:-1:1, binV in 8:-1:1, levelV in [10:10:90; 91]
-	@ptab "F-{:02d}-{:02d}-{:02d}", rackV, levelV, binV
+	@printf tab "F-%02d-%02d-%02d" rackV levelV binV
 	for rackH in 81:-1:1, binH in 8:-1:1, levelH in [10:10:90; 91]
-		@ptab "\t"
+		@printf tab "\t"
 		
 		# SAME RACK
 		if rackV==rackH
 			if binH == binV
-				@pcon "{:0.2f}", abs(n1to100(levelH) - n1to100(levelV)) / 100
+				@pcon @sprintf "A%0.2f" abs(n1to100(levelH) - n1to100(levelV)) / 100
 			end
-			@pcon "{:d}", abs(binH - binV)
+			@pcon @sprintf "B%d" abs(binH - binV)
 		end
 		
 		#SAME AISE
 		if aisle(rackV) == aisle(rackH)	
-			@pcon "{:d}", 8inAisleDists[1+mod(rackV, 6), 1+mod(rackH, 6)]+(binV-binH)
+			@pcon @sprintf "D%d" 8inAisleDists[1+mod(rackV, 6), 1+mod(rackH, 6)]+(binV-binH)
 		end
 		
 		#SAME FORE/AFT
-		#if fore_aft(rackV) == fore_aft(rackH)
+		if fore_aft(rackV) == fore_aft(rackH)
 			ad = abs(aisle(rackV) - aisle(rackH))
-			@pcon "{:d}", 8ad + (binV-binH) + 8inAisleDists[4, 1+mod(rackH, 6)]
-		#end
+			@pcon @sprintf "E%d" 8ad
+		end
 		
 	end
-	@ptab "\n"
+	@printf tab "\n"
 end
 
 close(tab)
-close(img)
 
