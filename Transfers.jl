598c4478e34dcdc9c168cfe3453ef2c4cb8c2e27
lawless-m <maht0x0r@xsmail.com>
2016-10-24 12:32:50 -0400

    check positions

--- Transfers.jl
+++ Transfers.jl
@@ -22,6 +22,29 @@ function FLabels(racks, bins, levels)
 	[@sprintf("F-%s-%02d-%02d", r, l, b) for r in racks, b in bins, l in levels]
 end
 
+function BLabels()
+	[ []
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 1:8, l in 10:10:60, b in [1:24; 31:55]])
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 9:19, l in 10:10:60, b in 1:25] 	)
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 2:3, l in 70:70, b in [1:24; 31:55]]  	)
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 4:4, l in 70:70, b in [17:24; 31:55]] )
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 5:7, l in 70:70, b in 31:55]  )
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 8:11, l in 70:70, b in 1:24]  )
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 13:13, l in 70:70, b in 1:8]  )
+	
+	]
+end
+
+function chanelLabels()
+	[ []
+	
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 15:15, l in [120; 140], b in 1:90]  )
+	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 15:15, l in [130], b in 1:120]  )
+
+	]
+
+end
+
 function printLods(fid, labels, lods)
 	for label in labels
 		if haskey(lods, string(locskus[label]))
@@ -82,8 +105,28 @@ function checkRacks()
 	end
 end
 
+function bakerStatus(fid, labels)
+	empty = 0
+	exist = 0
+	testers = 0
+	for label in sort(labels)
+		exist += 1
+		if haskey(currStolocs, label)
+			item = currStolocs[label][1]
+			@printf fid "%s\t(%s) %s\t%d\n" label item.prtnum item.descr item.qty
+			if item.descr[1:2] == "T "
+				testers += 1
+			end
+		else
+			empty += 1
+			@printf fid "%s\tEMPTY\n" label
+		end
+	end
+	@printf fid "Testers: %d / %d = %d%%\n" testers exist round(100testers/exist)
+	@printf fid "Empty: %d / %d = %d%%\n" empty exist round(100empty/exist)
+end
 
-j = 4
+j = 5
 if j==1
 	@fid "transfers/ALL-F.txt" procRacks(fid, [10:10:90; 91])
 elseif j==2
@@ -92,6 +135,13 @@ elseif j==3
 	checkRacks(locskus)
 elseif j == 4
 	@fid "transfers/Testers.txt"	printBRItems(fid, testerInNonTestBin)
+elseif j==5
+	d = Dates.format(today(), "u_d")
+	@fid "transfers/Status_$d.txt" bakerStatus(fid, BLabels())
+elseif j==6
+	d = Dates.format(today(), "u_d")
+	@fid "transfers/Status_Chanel_$d.txt" bakerStatus(fid, chanelLabels())
+
 end
 
 
