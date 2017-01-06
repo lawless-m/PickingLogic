f5a59b7241fa13c591305e60c029ea898475660c
lawless-m <maht0x0r@xsmail.com>
2017-01-06 10:50:21 -0500

    Revert "added date to filename"
    This reverts commit 9871524c24a32d3f6213c3c2c385ae02a96314d1.

--- HusLocs.jl
+++ HusLocs.jl
@@ -84,8 +84,7 @@ function allstolocs()
 	stolocs = stolocsHUS()
 	racks = racklist(stolocs)
 	allrow = 1
-	d = Dates.format(today(), "u_d")
-	@Xls "HUSLocs_$d" begin
+	@Xls "HUSLocs" begin
 		Cols = [("Area", 10) ("Loc", 12) ("prtnum", 10) ("Descr", 35) ("Qty", 5) ("Fixed Loc", 11) ("Typecode", 12) ("Category", 25)]
 		bold = add_format!(xls ,Dict("bold"=>true))
 		date_format = add_format!(xls, Dict("num_format"=>"d mmmm yyyy"))
