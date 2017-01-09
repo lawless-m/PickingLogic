3cea7fc4ef3104a98ce534f4e76eaa9b3340ad7f
lawless-m <maht0x0r@xsmail.com>
2017-01-09 13:12:08 -0500

    ignore scratch file

--- c.jl
+++ c.jl
@@ -1,15 +1,18 @@
-function BLabels()
-	[ []
-	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 1:1, l in 10:10:60, b in [1:24; 31:55]])	
-	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 3:8, l in 10:10:60, b in [1:24; 31:55]])	
-	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 9:19, l in 10:10:60, b in 1:25] 	)
-	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 2:2, l in 70:70, b in 31:55]  	)
-	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 3:3, l in 70:70, b in [1:24; 31:55]]  	)
-	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 4:4, l in 70:70, b in [17:24; 31:55]] )
-	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 5:7, l in 70:70, b in 31:55]  )
-	;vec(	[@sprintf("%02d-%02d-%02d", r, l, b) for r in 8:11, l in 70:70, b in 1:24]  )	
-	]
-end
 
-BLabels()
+cd(ENV["USERPROFILE"] * "/Documents")
+unshift!(LOAD_PATH, abspath("GitHub/PickingLogic/"))
+
+
+using HIARP
+
+
+include("utils.jl")
+include("merch_cats.jl")
+
+skulocs, locskus, rackskus = skuLocations()
+
+curr = currentStolocs()
+typecodes = typeCodesSerial()
+
+println(typecodes)
 
